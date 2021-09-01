24af3b5f998e259642de5403e94b105bcea37500
Marcos <mvinicius.oliveira04@gmail.com>
2021-08-31 22:14:09 -0300

    hoje eu lembrei

---
+++ Sprint 2 - Back-End/01.filmes/senai_filmes_webAPI/senai_filmes_webAPI/Domains/FilmeDomain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade(tabela) FILME
+    /// </summary>
+    public class FilmeDomain
+    {
+        public int idFilme { get; set; }
+        public int idGenero { get; set; }
+        public string tituloFilme { get; set; }
+    }
+}
