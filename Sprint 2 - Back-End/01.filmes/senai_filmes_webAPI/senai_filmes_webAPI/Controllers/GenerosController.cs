156c0d702ab7cdbdf9bc4d17a07490340b8c8528
Marcos <mvinicius.oliveira04@gmail.com>
2021-09-01 11:28:20 -0300

    api

---
+++ Sprint 2 - Back-End/01.filmes/senai_filmes_webAPI/senai_filmes_webAPI/Controllers/GenerosController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai_filmes_webAPI.Domains;
+using senai_filmes_webAPI.Interfaces;
+using senai_filmes_webAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/// <summary>
+/// Controlador responsável pelo end points referentes aos generos.
+/// </summary>
+
+namespace senai_filmes_webAPI.Controllers
+{
+    //Define que o tipo de resposta da API será no formato JSON
+    [Produces("applicaton/json")]
+
+    //Define que a rota de um requisição será no formato domínio/api/nomeController.
+    //ex: http://localhost:5000/api/generos
+    [Route("api/[controller]")]
+    //Define que é um Controlador de API.
+    [ApiController]
+    public class GenerosController : ControllerBase
+    {
+        /// <summary>
+        /// Objeto que irá receber todos os métodos definidos na interface
+        /// </summary>
+        private IGeneroRepository _GeneroRepository { get; set; }
+
+        /// <summary>
+        /// Instancia um objeto _GeneroRepository para que haja referencia dos métodos no repositório
+        /// </summary>
+        public GenerosController()
+        {
+            _GeneroRepository = new GeneroRepository();
+
+        }
+
+        [HttpGet]
+        //IActionResult = Resultado de uma acao.
+        //Get() = nome generico
+
+        public IActionResult Get()
+        {
+            //Lista de generos
+            //Se conectar com o Repositorio
+
+            //Criar uma lista nomeada listaGeneros para receber os dados.
+            List<GeneroDomain> ListaGeneros = _GeneroRepository.ListarTodos();
+
+            //Retorna os status code 200(OK) com a lista generos no formato JSON
+            return Ok(ListaGeneros);
+        }
+    }
+}
---
+++ Sprint 2 - Back-End/01.filmes/senai_filmes_webAPI/senai_filmes_webAPI/Repositories/GeneroRepository.cs
@@ -0,0 +1,91 @@
+﻿using senai_filmes_webAPI.Domains;
+using senai_filmes_webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Repositories
+{
+    public class GeneroRepository : IGeneroRepository
+    {
+        /// <summary>
+        /// String de conexão com banco de dados que receber parâmetros.
+        /// Data source= Nome do Servidor
+        /// Initial catalog = Nome do Banco de Dados
+        /// user ID= sa; pwd= Senai@132 = Faz autenticação com SQL SERVER passando o Login e Senha
+        /// Integrated Security=true = Faz autenticação com o usuario do sistema(windows)
+        /// </summary>
+        private string stringConexao = @"Data Source=NOTE0113C5; initial catalog=CATALOGO; user Id=sa; pwd=Senai@132";
+        public void AtualizarIdCorpo(GeneroDomain generoAtualizado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtualizarIdUrl(int idGenero, GeneroDomain generoAtualizado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GeneroDomain BuscarPorId(int idGenero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(GeneroDomain novoGenero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int idGenero)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Lista todos os gêneros
+        /// </summary>
+        /// <returns>Uma Lista de Gêneros</returns>
+        public List<GeneroDomain> ListarTodos()
+        {
+            List<GeneroDomain> listaGeneros = new List<GeneroDomain>();
+
+            //Declara a SqlConnectio con passando a stringConexao como parâmetro.
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string QuerySelectAll = "SELECT idGenero, nomeGenero FROM GENERO";
+
+                //Abre a conexão com banco de dados
+                con.Open();
+
+                //Declarando SqlDataReader rdrc percorrer a tabela do nosso banco de dados.
+                SqlDataReader rdr;
+
+                //Declara o SqlCommand passando da query que será executada e a conexão com parametros.
+                using (SqlCommand cmd = new SqlCommand(QuerySelectAll, con))
+                {
+                    //executa
+                    rdr = cmd.ExecuteReader();
+
+                    //Enquanto gouver registros para serem lidos no rdr, o laço se repete
+                    while(rdr.Read())
+                    {
+                        GeneroDomain genero = new GeneroDomain()
+                        {
+                            //atribui a propriedade idGenero o valor da primeira coluna do banco de dados.
+                            idGenero = Convert.ToInt32(rdr[0]),
+
+                            //atribui a propriedade nomeGenero o valor da segunda coluna do banco de dados.
+                            nomeGenero = rdr[1].ToString()
+                        };
+                        //adiciona o objeto genero criado a lista listaGeneros.
+                        listaGeneros.Add(genero);
+                    }
+                }
+            };
+
+            return listaGeneros;
+        }
+    }
+}
