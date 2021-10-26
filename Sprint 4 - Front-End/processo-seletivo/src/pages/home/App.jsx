bd71507d6405230fcf2d60b5d95e876107704db5
Marcaum <mvinicius.oliveira04@gmail.com>
2021-10-26 08:05:10 -0300

    css do processo seletivo terminado

--- Sprint 4 - Front-End/processo-seletivo/src/pages/home/App.jsx
+++ Sprint 4 - Front-End/processo-seletivo/src/pages/home/App.jsx
@@ -10,9 +10,6 @@ export default class Repositorios extends Component {
     }
   };
 
-  componentDidMount() {
-  }
-
   atualizaEstadoPesquisa = async (event) => {
     await this.setState({
       nomeUsuario: event.target.value
@@ -26,7 +23,7 @@ export default class Repositorios extends Component {
 
     console.log('agora vamos fazer a chamada para a api.')
 
-    fetch(`//api.github.com/users/${this.state.nomeUsuario}/repos?per_page=10`)
+    fetch(`https://api.github.com/users/${this.state.nomeUsuario}/repos?per_page=10`)
       .then(resposta => resposta.json())
       .then(dados => this.setState({ listaRepositorios: dados }))
       .catch(erro => console.log(erro))
@@ -39,16 +36,14 @@ export default class Repositorios extends Component {
       <div>
         <header className='header'>
           <h1>GitHub - Repositories</h1>
-          <hr />
         </header>
         <main>
           <section className="section-1">
             <h2>Usuário a ser buscado</h2>
             <form onSubmit={this.buscarUsuarioRepositorios}>
               <input onChange={this.atualizaEstadoPesquisa} type="text" placeholder="   Digite um usuário" />
-              <button type="submit" disable={this.state.nomeUsuario === '' ? 'none' : ''}>Buscar</button>
+              <button type="submit" disabled={this.state.nomeUsuario === '' ? 'none' : ''}>Buscar</button>
             </form>
-            <hr />
           </section>
           <section className="section-2">
             <h2>Lista de Repositorios</h2>
